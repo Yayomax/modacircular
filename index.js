af584390c022222cc6dbf1f0baf57df3d5574b42
IGabii <128262156+IGabii@users.noreply.github.com>
2023-05-23 11:47:13 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,20 +1,5 @@
 
 //barra lateral//
-var menuItem=document.querySelectorAll(`.item-menu`)
-
-function selectLink(){
-    menuItem.forEach((item)=>
-        item.classList.remove(`activo`)
-    )
-    this.classList.add(`activo`)
-}
-
-menuItem.forEach((item)=>
-    item.addEventListener(`click`,selectLink)
-)
-
-
-
 var btnExp=document.querySelector(`#btn-exp`)
 var menuSide=document.querySelector(`.menu-lateral`)
 
