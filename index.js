70567c189b9b2768370a34bc2ff9eda399da65a2
Yayomax <121267960+Yayomax@users.noreply.github.com>
2023-05-26 14:22:57 -0300

    devolvi todo

---
+++ index.js
@@ -0,0 +1,9 @@
+
+//barra lateral//
+var btnExp=document.querySelector(`#btn-exp`)
+var menuSide=document.querySelector(`.menu-lateral`)
+
+btnExp.addEventListener(`click`,function(){
+    menuSide.classList.toggle(`expandir`)
+}
+)
