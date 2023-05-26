28cd6e61387b4dc8855730bf03478783a93f5aae
Yayomax <121267960+Yayomax@users.noreply.github.com>
2023-05-26 14:12:26 -0300

    Add files via upload

--- index.js
+++
@@ -1,9 +0,0 @@
-
-//barra lateral//
-var btnExp=document.querySelector(`#btn-exp`)
-var menuSide=document.querySelector(`.menu-lateral`)
-
-btnExp.addEventListener(`click`,function(){
-    menuSide.classList.toggle(`expandir`)
-}
-)
